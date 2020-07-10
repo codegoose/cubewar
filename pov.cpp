fd5ce1e1203ea34d4f8bc25452ebc4830f19c469
Brandon White <b.white.1@pm.me>
2020-07-10 09:34:59 -0700

    Increase FOV.

--- pov.cpp
+++ pov.cpp
@@ -6,7 +6,7 @@ glm::vec3 cw::pov::eye;
 glm::vec3 cw::pov::center;
 glm::vec3 cw::pov::look;
 glm::vec2 cw::pov::orientation;
-float cw::pov::field_of_view = glm::radians(70.0f);
+float cw::pov::field_of_view = glm::radians(85.0f);
 float cw::pov::near_plane_distance = 0.01f;
 float cw::pov::far_plane_distance = 400.01f;
 glm::mat4 cw::pov::view_matrix = glm::identity<glm::mat4>();
