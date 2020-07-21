45d52e4f271b5e96450362c5ee6eec577f27ac2f
Brandon White <b.white.1@pm.me>
2020-07-20 17:08:38 -0700

    Add SSAO. Fix lighting calculations.

--- build/default/glsl/screen.fs
+++ build/default/glsl/screen.fs
@@ -86,16 +86,12 @@ vec3 promo_outline(vec3 color) {
 	return color;
 }
 
-// ---
-
-// Desaturate function:
 vec4 desaturate(vec4 iColor, float iExtent) {
 	vec3 tCoeff = vec3( 0.3, 0.59, 0.11 );
 	vec3 tGray  = vec3( dot( tCoeff, iColor.rgb ) );
 	return vec4( mix( iColor.rgb, tGray, iExtent ), iColor.a );
 }
 
-// Contrast, saturation and brightness adjustment function:
 vec4 contrastSaturationBrightness(vec4 iColor, float iBrightness, float iSaturation, float iContrast) {
 	const vec3 tLumAvg   = vec3( 0.5, 0.5, 0.5 );
 	const vec3 tLumCoeff = vec3( 0.2125, 0.7154, 0.0721 );
@@ -106,7 +102,6 @@ vec4 contrastSaturationBrightness(vec4 iColor, float iBrightness, float iSaturat
 	return vec4( tConClr, iColor.a );
 }
 
-// RGB to HSL colorspace conversion function:
 vec3 rgbToHsl(vec3 iColor) {
 	vec3  tHsl   = vec3( 0.0 );
 	float tMin   = min( min( iColor.r, iColor.g), iColor.b );
@@ -128,7 +123,6 @@ vec3 rgbToHsl(vec3 iColor) {
 	return tHsl;
 }
 
-// Hue to RGB colorspace conversion helper function:
 float hueToRgb(float iF1, float iF2, float iHue) {
 	if( iHue < 0.0 )      { iHue += 1.0; }
 	else if( iHue > 1.0 ) { iHue -= 1.0; }
@@ -140,7 +134,6 @@ float hueToRgb(float iF1, float iF2, float iHue) {
 	return tRes;
 }
 
-// HSL to RGB colorspace conversion function:
 vec3 hslToRgb(vec3 iHsl) {
 	vec3 tRgb;
 	if( iHsl.y == 0.0 ) {
@@ -158,52 +151,123 @@ vec3 hslToRgb(vec3 iHsl) {
 	return tRgb;
 }
 
-// Combines luminance and saturation of iBaseColor with hue of iBlendColor:
 vec3 blendHue(vec3 iBaseColor, vec3 iBlendColor) {
 	vec3 tBase = rgbToHsl( iBaseColor );
 	return hslToRgb( vec3( rgbToHsl(iBlendColor).r, tBase.g, tBase.b ) );
 }
 
-// Combines luminance and hue of iBaseColor with the saturation of iBlendColor:
 vec3 blendSaturation(vec3 iBaseColor, vec3 iBlendColor) {
 	vec3 tBase = rgbToHsl( iBaseColor );
 	return hslToRgb( vec3( tBase.r, rgbToHsl(iBlendColor).g, tBase.b ) );
 }
 
-// Combines hue and saturation of iBaseColor with the luminance of iBlendColor:
 vec3 blendLuminosity(vec3 iBaseColor, vec3 iBlendColor) {
 	vec3 tBase = rgbToHsl( iBaseColor );
 	return hslToRgb( vec3( tBase.r, tBase.g, rgbToHsl(iBlendColor).b ) );
 }
 
-// Uses brightness of iBaseColor with the hue and saturation of iBlendColor:
 vec3 blendColor(vec3 iBaseColor, vec3 iBlendColor) {
 	vec3 tBlend = rgbToHsl( iBlendColor );
 	return hslToRgb( vec3( tBlend.r, tBlend.g, rgbToHsl(iBaseColor).b ) );
 }
 
-// ---
+#define SAMPLES 8
+#define INTENSITY 0.75
+#define SCALE 5
+#define BIAS 0.05
+#define SAMPLE_RAD 0.5
+#define MAX_DISTANCE 0.5
+#define MOD3 vec3(.1031,.11369,.13787)
+
+float hash12(vec2 p)
+{
+	vec3 p3  = fract(vec3(p.xyx) * MOD3);
+    p3 += dot(p3, p3.yzx + 19.19);
+    return fract((p3.x + p3.y) * p3.z);
+}
+
+vec2 hash22(vec2 p)
+{
+	vec3 p3 = fract(vec3(p.xyx) * MOD3);
+    p3 += dot(p3, p3.yzx+19.19);
+    return fract(vec2((p3.x + p3.y)*p3.z, (p3.x+p3.z)*p3.y));
+}
+
+vec3 getPosition(vec2 uv) {
+    return texture2D(deferred_position_buffer, uv).rgb;
+}
+
+vec3 getNormal(vec2 uv) {
+    return texture2D(deferred_surface_buffer, uv).rgb;;
+}
+
+vec2 getRandom(vec2 uv) {
+    return normalize(hash22(uv*126.1231) * 2. - 1.);
+}
+
+
+float doAmbientOcclusion(in vec2 tcoord,in vec2 uv, in vec3 p, in vec3 cnorm)
+{
+    vec3 diff = getPosition(tcoord + uv) - p;
+    float l = length(diff);
+    vec3 v = diff/l;
+    float d = l*SCALE;
+    float ao = max(0.0,dot(cnorm,v)-BIAS)*(1.0/(1.0+d));
+    ao *= smoothstep(MAX_DISTANCE,MAX_DISTANCE * 0.5, l);
+    return ao;
+
+}
+
+float spiralAO(vec2 uv, vec3 p, vec3 n, float rad)
+{
+    float goldenAngle = 2.4;
+    float ao = 0.;
+    float inv = 1. / float(SAMPLES);
+    float radius = 0.;
+
+    float rotatePhase = hash12( uv*100. ) * 6.28;
+    float rStep = inv * rad;
+    vec2 spiralUV;
+
+    for (int i = 0; i < SAMPLES; i++) {
+        spiralUV.x = sin(rotatePhase);
+        spiralUV.y = cos(rotatePhase);
+        radius += rStep;
+        ao += doAmbientOcclusion(uv, spiralUV * radius, p, n);
+        rotatePhase += goldenAngle;
+    }
+    ao *= inv;
+    return ao;
+}
 
 vec3 get_diffuse(vec2 uv) {
 	vec4 material_coords = texture2D(deferred_material_buffer, uv);
 	if (material_coords.a == 0) return vec3(0, 0, 0); // sky
 	vec3 normal = texture2D(deferred_surface_buffer, uv).rgb;
-	float light_dot = max(dot(normal, normalize(vec3(0.25, 0.25, 1))), 0);
-	float light_power = (0.7 + (light_dot * 1.2));
+	float light_dot = max(dot(normal, normalize(vec3(0.2, 0.1, 1))), 0);
 	vec3 world_position = texture2D(deferred_position_buffer, uv).rgb;
 	vec3 shadow_map_uv = vec4(sun_shadow_matrix * vec4(world_position, 1)).rgb;
 	float world_sun_depth = (shadow_map_uv.z + 1.0) * 0.5;
 	shadow_map_uv += 1.0;
 	shadow_map_uv *= 0.5;
 	float shadow_map_sample = texture2D(sun_shadow_map, shadow_map_uv.xy).r;
-	float shadow_map_sample_2 = texture2D(sun_shadow_map, shadow_map_uv.xy + vec2(1.0 / 2048.0, 0)).r;
-	float shadow_map_sample_3 = texture2D(sun_shadow_map, shadow_map_uv.xy - vec2(1.0 / 2048.0, 0)).r;
+	float light_power;
 	if (shadow_map_uv.x >= 0.0 && shadow_map_uv.x <= 1.0 && shadow_map_uv.y >= 0.0 && shadow_map_uv.y <= 1.0) {
-		if (world_sun_depth - 0.0035 >= shadow_map_sample) light_power = 0.7;
-	}
-	if (material_coords.a == 80000) {
-		return contrastSaturationBrightness(texture(voxel_array, material_coords.rgb), light_power, saturation_power, 1.0).rgb;
-	} else return contrastSaturationBrightness(vec4(resolve_material_diffuse(material_coords.a), 1), light_power, saturation_power, 1.0).rgb;
+		if (world_sun_depth - 0.0035 >= shadow_map_sample) { 
+		    float ao = 0.;
+		    float rad = SAMPLE_RAD / world_position.z;
+		    ao = spiralAO(sh_uv, world_position, getNormal(sh_uv), rad);
+		    ao = 1. - ao * INTENSITY;
+			light_power = 0.65 * ao;
+		} else {
+			light_power = (0.7 + (light_dot * 2.0));
+		}
+	} else light_power = (0.7 + (light_dot * 2.0));
+	//
+	vec3 texture_color;
+	if (material_coords.a == 80000) texture_color = texture(voxel_array, material_coords.rgb).rgb;
+	else texture_color = resolve_material_diffuse(material_coords.a);
+	return contrastSaturationBrightness(vec4(texture_color, 1), light_power, saturation_power, 1.0).rgb;
 }
 
 vec3 get_sharpened(vec3 color, vec2 coords) {
